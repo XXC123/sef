6ab0349cc7106e034797e21a9f20e0ed905718ac
XXC123 <36947310+XXC123@users.noreply.github.com>
2018-04-04 19:18:11 +1000

    add UI and profile function
    add basic menu.
    implement the modify profile function.

--- HRApplication.java
+++ HRApplication.java
@@ -1,7 +1,10 @@
 package sefA1;
 
+import java.util.Scanner;
+
 public class HRApplication {
 
+	Scanner scanner=new Scanner(System.in);
 	
 	
 	public void login()
@@ -9,6 +12,206 @@ public class HRApplication {
 	//to-do	
 	}
 	
+	public void run() {
+		// TODO Auto-generated method stub
+		adminUI();
+	}
+	
+	public void adminUI()
+	{
+		System.out.println("-----------Menu-----------");
+		System.out.println("1.Message other staff");
+		System.out.println("2.Check announcements/ notifications");
+		System.out.println("3.Check pay rates ");
+		System.out.println("4.See weekly roster");
+		System.out.println("5.Update profile");
+		System.out.println("6.Gather report data");
+		System.out.println("7.View all staff");
+		System.out.println("8.View course staff requests");
+		System.out.println("9.Set rosters");
+		System.out.println("0.Allocate staff");
+		int selection= scanner.nextInt();
+		switch(selection)
+		{
+			case 1:
+				message();
+				break;
+			case 2:
+				viewAnnouncements();
+				break;
+			case 3:
+				checkPayrate();
+				break;
+			case 4:
+				seeRoster();
+				break;
+			case 5:
+				viewProfile();
+				break;
+			case 6:
+				gatherPayroll();
+				break;
+			case 7:
+				viewStaff();
+				break;
+			case 8:
+				viewCourseStaffRequest();
+				break;
+			case 9:
+				setRoster();
+				break;
+			case 0:
+				allocateStaff();
+				break;
+		}
+	}
+	
+	
+	public void coordinatorUI()
+	{
+		System.out.println("1.Message other staff");
+		System.out.println("2.Check announcements/ notifications");
+		System.out.println("3.Check pay rates");
+		System.out.println("4.See weekly roster");
+		System.out.println("5.Update profile");
+		System.out.println("6.See course budget");
+		System.out.println("7.Set payrate for event");
+		System.out.println("8.Class timetabling");
+		System.out.println("9.Request staff for events");
+		System.out.println("0.Make course anouncement");
+		
+		int selection= scanner.nextInt();
+		switch(selection)
+		{
+			case 1:
+				message();
+				break;
+			case 2:
+				viewAnnouncements();
+				break;
+			case 3:
+				checkPayrate();
+				break;
+			case 4:
+				seeRoster();
+				break;
+			case 5:
+				viewProfile();
+				break;
+			case 6:
+				seeCourseBudget();
+				break;
+			case 7:
+				approveStaffPay();
+				break;
+			case 8:
+				createCourseEvent();
+				break;
+			case 9:
+				StaffForEvent();
+				break;
+			case 0:
+				Announcement();
+				break;
+		}
+	}
+	
+	
+	public void casualUI()
+	{
+		System.out.println("1.Message other staff");
+		System.out.println("2.Check announcements/ notifications");
+		System.out.println("3.Check pay rates ");
+		System.out.println("4.See weekly roster");
+		System.out.println("5.Update profile");
+		System.out.println("6.Update resume");
+		System.out.println("7.Apply for positions");
+		
+		int selection= scanner.nextInt();
+		switch(selection)
+		{
+			case 1:
+				message();
+				break;
+			case 2:
+				viewAnnouncements();
+				break;
+			case 3:
+				checkPayrate();
+				break;
+			case 4:
+				seeRoster();
+				break;
+			case 5:
+				viewProfile();
+				break;
+			case 6:
+				gatherPayroll();
+				break;
+			case 7:
+				viewStaff();
+				break;
+			case 8:
+				viewCourseStaffRequest();
+				break;
+			case 9:
+				setRoster();
+				break;
+			case 0:
+				allocateStaff();
+				break;
+		}
+	
+
+	}
+	
+	public void timeManagerUI()
+	{
+		System.out.println("1.Message other staff");
+		System.out.println("2.Check announcements/ notifications");
+		System.out.println("3.Check pay rates");
+		System.out.println("4.See weekly roster");
+		System.out.println("5.Update profile");
+		System.out.println("6.Gather weekly payroll data");
+		System.out.println("7.Approve staff weekly pay");
+
+		
+		int selection= scanner.nextInt();
+		switch(selection)
+		{
+			case 1:
+				message();
+				break;
+			case 2:
+				viewAnnouncements();
+				break;
+			case 3:
+				checkPayrate();
+				break;
+			case 4:
+				seeRoster();
+				break;
+			case 5:
+				viewProfile();
+				break;
+			case 6:
+				gatherPayroll();
+				break;
+			case 7:
+				viewStaff();
+				break;
+			case 8:
+				viewCourseStaffRequest();
+				break;
+			case 9:
+				setRoster();
+				break;
+			case 0:
+				allocateStaff();
+				break;
+		}
+	
+	}
 	
 	public void message()
 	{
@@ -48,7 +251,9 @@ public class HRApplication {
 	}
 	public void gatherPayroll()
 	{
-	//to-do	
+	//to-do
+		Report report=new Report();
+		report.generateReport();
 	}
 	public void approveStaffPay()
 	{
@@ -74,7 +279,50 @@ public class HRApplication {
 	}
 	public void viewProfile()
 	{
-	//to-do	
+	Profile profile= new Profile();
+	profile.viewProfile();
+	
+	System.out.println("-------Modify your profile:-------");
+	System.out.println("1.Modify whole profile");
+	System.out.println("2.Modify one featrue");
+	
+	int selection= scanner.nextInt();
+	switch(selection)
+	{
+		case 1:
+			profile.modifyProfile();
+			break;
+		case 2:
+			
+			System.out.println("1.Modify name");
+			System.out.println("2.Modify dob");
+			System.out.println("3.Modify phone");
+			System.out.println("4.Modify email");
+			
+			selection= scanner.nextInt();
+			switch(selection)
+			{
+			case 1:
+				System.out.println("Input new name");
+				profile.setName(scanner.next());
+				break;
+			case 2:
+				System.out.println("Input new dob");
+				profile.setDob(scanner.next());
+				break;
+			case 3:
+				System.out.println("Input new phone");
+				profile.setPhone(scanner.next());
+				break;
+			case 4:
+				System.out.println("Input new email");
+				profile.setEmail(scanner.next());
+				break;
+			}
+			break;
 	}
+	profile.viewProfile();
+		
 	}
+}
 
--- Profile.java
+++ Profile.java
@@ -1,19 +1,18 @@
 package sefA1;
 
+import java.util.Scanner;
+
 public class Profile {
 
+	Scanner scanner=new Scanner(System.in);
 	
 	private String name;
 	private String dob;
 	private String phone;
 	private String email;
 
-	public void Profile(String name, String dob, String phone, String email)
+	public void Profile()
 	{
-		this.name=name;
-		this.dob=dob;
-		this.phone=phone;
-		this.email=email;
 	}
 	
 	public void updateProfile()
@@ -21,9 +20,64 @@ public class Profile {
 	//to-do	
 	}
 	
+	public void viewProfile()
+	{
+		System.out.println("Name: "+this.name);
+		System.out.println("Date of birth: "+this.dob);
+		System.out.println("Phone: "+this.phone);
+		System.out.println("Email: "+this.email);
+	}
+	
 	public void updateResume()
 	{
 	//to-do	
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void modifyProfile() {
+		System.out.println("Input new name");
+		this.name=scanner.next();
+		
+		System.out.println("Input new dob");
+		this.dob=scanner.next();
+		
+		System.out.println("Input new phone");
+		this.phone=scanner.next();
+		
+		System.out.println("Input new email");
+		this.email=scanner.next();
+		
 	}
 	
 }
